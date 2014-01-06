233d44e43a60245b32f6b32ebae30c3ce3c95f64
Even Lislebø <e.lislebo@gmail.com>
2014-01-06 20:36:03 +0100

    Minor

--- euler/p12.hs
+++ euler/p12.hs
@@ -1,4 +1,4 @@
 import Data.List
 import EulerUtils
-p11 = head [a | a <- triangleStream, divisors a > 500]
+p12 = head [a | a <- triangleStream, divisors a > 500]
   where divisors x = product (map ((+1).length) (group (primeFactors x)))
